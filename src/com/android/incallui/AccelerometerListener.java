d7a1aa6abb2c91de804afd1a4c0c6f847afa3c24
Sasikumar D R <sasikumardr@gmail.com>
2014-01-07 21:46:10 +0530

    [3/3] Flip to Mute/Reject Call
    Actual Logic for this functionality.
    
    PS2: Remove unwanted space
    
    Change-Id: Ia10df0045e9919c6b7efc97857abaeba246c365d

--- src/com/android/incallui/AccelerometerListener.java
+++ src/com/android/incallui/AccelerometerListener.java
@@ -16,13 +16,21 @@
 
 package com.android.incallui;
 
+import java.lang.reflect.Method;
+
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
+import com.android.internal.telephony.ITelephony;
+import com.android.services.telephony.common.Call;
+
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
@@ -37,6 +45,7 @@ public final class AccelerometerListener {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private ITelephony mTelephonyService;
 
     // mOrientation is the orientation value most recently reported to the client.
     private int mOrientation;
@@ -59,14 +68,43 @@ public final class AccelerometerListener {
     private static final int HORIZONTAL_DEBOUNCE = 500;
     private static final double VERTICAL_ANGLE = 50.0;
 
+ // Our accelerometers are not quite accurate.
+    private static final int FACE_UP_GRAVITY_THRESHOLD = 7;
+    private static final int FACE_DOWN_GRAVITY_THRESHOLD = -7;
+    private static final int TILT_THRESHOLD = 3;
+    private static final int SENSOR_SAMPLES = 3;
+    private static final int MIN_ACCEPT_COUNT = SENSOR_SAMPLES - 1;
+
+ // Flip action IDs
+    private static final int RINGING_NO_ACTION = 0;
+    private static final int MUTE_RINGER = 1;
+    private static final int DISMISS_CALL = 2;
+
+    private boolean mStopped;
+    private boolean mWasFaceUp;
+    private boolean[] mSamples = new boolean[SENSOR_SAMPLES];
+    private int mSampleIndex;
+    private Context mContext;
+    private InCallPresenter mInCallPresenter;
+
     public interface OrientationListener {
         public void orientationChanged(int orientation);
+    }
+
+    private interface ResettableSensorEventListener extends SensorEventListener {
+        public void reset();
     }
 
     public AccelerometerListener(Context context, OrientationListener listener) {
         mListener = listener;
         mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public AccelerometerListener(Context context){
+        mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        mContext = context;
+        mTelephonyService = getTeleService();
     }
 
     public void enable(boolean enable) {
@@ -81,6 +119,44 @@ public final class AccelerometerListener {
                 mSensorManager.unregisterListener(mSensorListener);
                 mHandler.removeMessages(ORIENTATION_CHANGED);
             }
+        }
+    }
+
+    public void enableSensor(boolean enable) {
+        if (DEBUG) Log.d(TAG, "enableSensor(" + enable + ")");
+        int action = getFlipAction();
+        synchronized (this) {
+            if (enable) {
+                if (action != RINGING_NO_ACTION) {
+                    mFlipListener.reset();
+                    mSensorManager.registerListener(mFlipListener,
+                            mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                            SensorManager.SENSOR_DELAY_NORMAL);
+                }
+            } else {
+                if (action != RINGING_NO_ACTION) {
+                    mSensorManager.unregisterListener(mFlipListener);
+                }
+            }
+        }
+    }
+
+    private ITelephony getTeleService() {
+        TelephonyManager tm = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            // Java reflection to gain access to TelephonyManager's
+            // ITelephony getter
+            Log.v(TAG, "Get getTeleService...");
+            Class c = Class.forName(tm.getClass().getName());
+            Method m = c.getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+            return (ITelephony) m.invoke(tm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG,
+                    "FATAL ERROR: could not connect to telephony subsystem");
+            Log.e(TAG, "Exception object: " + e);
+            return null;
         }
     }
 
@@ -139,6 +215,119 @@ public final class AccelerometerListener {
             // ignore
         }
     };
+
+    private final ResettableSensorEventListener mFlipListener = new ResettableSensorEventListener() {
+        private static final String TAG = "FlipListener";
+        // Our accelerometers are not quite accurate.
+        private static final int FACE_UP_GRAVITY_THRESHOLD = 7;
+        private static final int FACE_DOWN_GRAVITY_THRESHOLD = -7;
+        private static final int TILT_THRESHOLD = 3;
+        private static final int SENSOR_SAMPLES = 3;
+        private static final int MIN_ACCEPT_COUNT = SENSOR_SAMPLES - 1;
+
+        private boolean mStopped;
+        private boolean mWasFaceUp;
+        private boolean[] mSamples = new boolean[SENSOR_SAMPLES];
+        private int mSampleIndex;
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int acc) {
+        }
+
+        @Override
+        public void reset() {
+            Log.d(TAG, "FlipListener Reset()");
+            mWasFaceUp = false;
+            mStopped = false;
+            for (int i = 0; i < SENSOR_SAMPLES; i++) {
+                mSamples[i] = false;
+            }
+        }
+
+        private boolean filterSamples() {
+            int trues = 0;
+            for (boolean sample : mSamples) {
+                if(sample) {
+                    ++trues;
+                }
+            }
+            return trues >= MIN_ACCEPT_COUNT;
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // Add a sample overwriting the oldest one. Several samples
+            // are used to avoid the erroneous values the sensor sometimes
+            // returns.
+            float z = event.values[2];
+
+            if (mStopped) {
+                return;
+            }
+
+            if (!mWasFaceUp) {
+                // Check if its face up enough.
+                mSamples[mSampleIndex] = z > FACE_UP_GRAVITY_THRESHOLD;
+
+                // face up
+                if (filterSamples()) {
+                    Log.d(TAG, "onSensorChanged() - Face Up");
+                    mWasFaceUp = true;
+                    for (int i = 0; i < SENSOR_SAMPLES; i++) {
+                        mSamples[i] = false;
+                    }
+                }
+            } else {
+                // Check if its face down enough.
+                mSamples[mSampleIndex] = z < FACE_DOWN_GRAVITY_THRESHOLD;
+
+                // face down
+                if (filterSamples()) {
+                    Log.d(TAG, "onSensorChanged() - Face Down");
+                    mStopped = true;
+                    handleAction();
+                }
+            }
+
+            mSampleIndex = ((mSampleIndex + 1) % SENSOR_SAMPLES);
+        }
+    };
+
+    public void handleAction() {
+        int action = getFlipAction();
+        Log.d(TAG, "handleAction() - Flip Action : " + action);
+
+        switch(action) {
+            case MUTE_RINGER:
+                if(mTelephonyService != null){
+                    try{
+                        mTelephonyService.silenceRinger();
+                    }catch(android.os.RemoteException e){
+                        Log.d(TAG, e.toString());
+                    }
+                }
+                break;
+            case DISMISS_CALL:
+                endCall();
+                break;
+            case RINGING_NO_ACTION:
+            default:
+                //no action
+                break;
+        }
+    }
+
+    private void endCall(){
+        final CallList calls = CallList.getInstance();
+        Call iCall = calls.getIncomingCall();
+        CallCommandClient.getInstance().rejectCall(iCall, false, null);
+    }
+
+    private int getFlipAction(){
+        int flipAction = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FLIP_ACTION_KEY,0);
+        return flipAction;
+    }
 
     Handler mHandler = new Handler() {
         public void handleMessage(Message msg) {
