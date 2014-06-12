72361e4c0dd15c092de8473f3f57fc27e11adf7d
maxwen <max.weninger@gmail.com>
2014-06-12 02:33:36 +0200

    InCallUI: non intrusive incall ui rework
    Use glow pad with accept and reject target
    Added button to open fullscreen ui
    
    Change-Id: I12ea058ed970b13608c5abfa8c0cd535dfc621ae

--- src/com/android/incallui/InCallCardActivity.java
+++ src/com/android/incallui/InCallCardActivity.java
@@ -2,11 +2,16 @@ package com.android.incallui;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.DecelerateInterpolator;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,12 +25,12 @@ import com.android.services.telephony.common.Call;
  * Handles the call card activity that pops up when a call
  * arrives
  */
-public class InCallCardActivity extends Activity {
+public class InCallCardActivity extends Activity implements GlowPadWrapper.AnswerListener {
     private static final int SLIDE_IN_DURATION_MS = 500;
 
     private TextView mNameTextView;
     private ImageView mContactImage;
-
+    private Handler mHandler = new Handler();
     private Call mCall;
 
     @Override
@@ -43,31 +48,33 @@ public class InCallCardActivity extends Activity {
         final CallList calls = CallList.getInstance();
         mCall = calls.getIncomingCall();
 
-        CallIdentification identification = mCall.getIdentification();
+        final CallIdentification identification = mCall.getIdentification();
 
         // Setup the fields to show the information of the call
         mNameTextView = (TextView) findViewById(R.id.txt_contact_name);
         mContactImage = (ImageView) findViewById(R.id.img_contact);
 
-        // Setup the call button
-        Button answer = (Button) findViewById(R.id.btn_answer);
-        answer.setOnClickListener(new View.OnClickListener() {
+        ImageButton fullscreenUI = (ImageButton) findViewById(R.id.fullscreen_ui);
+        fullscreenUI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // start fullscreen ui
                 InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, false);
-                CallCommandClient.getInstance().answerCall(mCall.getCallId());
                 finish();
             }
         });
 
-        Button reject = (Button) findViewById(R.id.btn_reject);
-        reject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CallCommandClient.getInstance().rejectCall(mCall, false, null);
-                finish();
-            }
-        });
+        GlowPadWrapper glowpad = (GlowPadWrapper) findViewById(R.id.glow_pad_view);
+        // Answer or Decline.
+        glowpad.setTargetResources(R.array.incoming_call_widget_2way_targets);
+        glowpad.setTargetDescriptionsResourceId(
+                R.array.incoming_call_widget_2way_target_descriptions);
+        glowpad.setDirectionDescriptionsResourceId(
+                R.array.incoming_call_widget_2way_direction_descriptions);
+        glowpad.reset(false);
+
+        glowpad.setAnswerListener(this);
+        glowpad.startPing();
 
         // Slide in the dialog
         final LinearLayout vg = (LinearLayout) findViewById(R.id.root);
@@ -76,8 +83,13 @@ public class InCallCardActivity extends Activity {
         vg.animate().translationY(0.0f).setDuration(SLIDE_IN_DURATION_MS)
             .setInterpolator(new DecelerateInterpolator()).start();
 
-        // Lookup contact info
-        startContactInfoSearch(identification);
+        // Lookup contact info - delay it a little bit the slide-in animation can be disturbed
+        // and we want it to look smooth
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                startContactInfoSearch(identification);
+            }
+        }, SLIDE_IN_DURATION_MS/2);
     }
 
     @Override
@@ -108,11 +120,45 @@ public class InCallCardActivity extends Activity {
                 public void onImageLoadComplete(int callId, ContactCacheEntry entry) {
                     if (entry.photo != null) {
                         Drawable current = mContactImage.getDrawable();
-                        AnimationUtils.startCrossFade(mContactImage, current, entry.photo);
+                        int contactHeight = getResources().getDimensionPixelSize(R.dimen.incoming_call_card_contact_height);
+                        Drawable photo = resize(entry.photo, contactHeight);
+                        AnimationUtils.startCrossFade(mContactImage, current, photo);
                     }
                 }
             });
     }
 
+    @Override
+    public void onAnswer() {
+        InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, false);
+        CallCommandClient.getInstance().answerCall(mCall.getCallId());
+        finish();
+    }
+
+    @Override
+    public void onDecline() {
+        CallCommandClient.getInstance().rejectCall(mCall, false, null);
+        finish();
+    }
+
+    @Override
+    public void onText() {
+    }
+
+    private BitmapDrawable resize(Drawable image, int dpiSize) {
+        float density = getResources().getDisplayMetrics().density;
+        int size = Math.round(dpiSize * density);
+
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+
+        Bitmap bmResult = Bitmap.createBitmap(size, size,
+                Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(bmResult);
+
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, size,
+                size, true);
+        tempCanvas.drawBitmap(bitmapResized, 0, 0, null);
+        return new BitmapDrawable(getResources(), bmResult);
+    }
 }
 
