178b1cb0353a3267ff38aba9cfc2324a2417eaef
maxwen <max.weninger@gmail.com>
2014-06-12 02:33:36 +0200

    InCallUI: non intrusive incall ui rework
    Use glow pad with accept and reject target
    Added button to open fullscreen ui
    
    PS8:
    -simple flat style again
    
    PS9: added some bells and whistles
    -added hide animation
    -added background button
    -added swipe down to background
    
    PS10;
    -added button pressed feedback
    
    Change-Id: I12ea058ed970b13608c5abfa8c0cd535dfc621ae

--- src/com/android/incallui/InCallCardActivity.java
+++ src/com/android/incallui/InCallCardActivity.java
@@ -2,11 +2,19 @@ package com.android.incallui;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.ViewConfiguration;
+import android.view.MotionEvent;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,17 +28,27 @@ import com.android.services.telephony.common.Call;
  * Handles the call card activity that pops up when a call
  * arrives
  */
-public class InCallCardActivity extends Activity {
+public class InCallCardActivity extends Activity implements GlowPadWrapper.AnswerListener {
     private static final int SLIDE_IN_DURATION_MS = 500;
+    private static final int SLIDE_OUT_DURATION_MS = 300;
+    private static final int SLIDE_SWIPE_DURATION_MS = 200;
 
     private TextView mNameTextView;
     private ImageView mContactImage;
-
+    private Handler mHandler = new Handler();
     private Call mCall;
+    private LinearLayout mCard;
+    private float mStartY;
+    private int mSlop;
+    private int mCardHeight;
 
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
+        ViewConfiguration vc = ViewConfiguration.get(this);
+        mSlop = vc.getScaledTouchSlop();
+        mCardHeight = getResources().getDimensionPixelSize(R.dimen.incoming_call_card_height);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
@@ -43,50 +61,122 @@ public class InCallCardActivity extends Activity {
         final CallList calls = CallList.getInstance();
         mCall = calls.getIncomingCall();
 
-        CallIdentification identification = mCall.getIdentification();
+        final CallIdentification identification = mCall.getIdentification();
 
         // Setup the fields to show the information of the call
         mNameTextView = (TextView) findViewById(R.id.txt_contact_name);
         mContactImage = (ImageView) findViewById(R.id.img_contact);
 
-        // Setup the call button
-        Button answer = (Button) findViewById(R.id.btn_answer);
-        answer.setOnClickListener(new View.OnClickListener() {
+        ImageView fullscreenUI = (ImageView) findViewById(R.id.fullscreen_ui);
+        fullscreenUI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // start fullscreen ui
                 InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, false);
-                CallCommandClient.getInstance().answerCall(mCall.getCallId());
                 finish();
             }
         });
 
-        Button reject = (Button) findViewById(R.id.btn_reject);
-        reject.setOnClickListener(new View.OnClickListener() {
+        ImageView dismissUI = (ImageView) findViewById(R.id.dismiss_ui);
+        dismissUI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CallCommandClient.getInstance().rejectCall(mCall, false, null);
-                finish();
+                // send to background
+                InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, true);
+                // slide out
+                finishWithHide();
             }
         });
+        GlowPadWrapper glowpad = (GlowPadWrapper) findViewById(R.id.glow_pad_view);
+        // Answer or Decline.
+        glowpad.setTargetResources(R.array.incoming_call_widget_2way_targets);
+        glowpad.setTargetDescriptionsResourceId(
+                R.array.incoming_call_widget_2way_target_descriptions);
+        glowpad.setDirectionDescriptionsResourceId(
+                R.array.incoming_call_widget_2way_direction_descriptions);
+        glowpad.reset(false);
+
+        glowpad.setAnswerListener(this);
+        glowpad.startPing();
 
         // Slide in the dialog
-        final LinearLayout vg = (LinearLayout) findViewById(R.id.root);
+        mCard = (LinearLayout) findViewById(R.id.root);
+        mCard.setOnTouchListener(new View.OnTouchListener() {
 
-        vg.setTranslationY(getResources().getDimensionPixelSize(R.dimen.incoming_call_card_height));
-        vg.animate().translationY(0.0f).setDuration(SLIDE_IN_DURATION_MS)
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                float yRaw = event.getRawY();
+
+                switch (action) {
+                case MotionEvent.ACTION_CANCEL:
+                    break;
+                case MotionEvent.ACTION_DOWN:
+                    mStartY = yRaw;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    // moved down more then 1/3 of the height
+                    if (yRaw - mStartY > mCardHeight / 3) {
+                        // send in background
+                        InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, true);
+                        // slide out fast
+                        mCard.animate().translationY(mCardHeight).setDuration(SLIDE_SWIPE_DURATION_MS)
+                                .setInterpolator(new AccelerateInterpolator()).start();
+                        mHandler.postDelayed(new Runnable() {
+                            public void run() {
+                                finish();
+                            }
+                        }, SLIDE_SWIPE_DURATION_MS);
+                    } else {
+                        // slide back
+                        mCard.animate().translationY(0.0f).setDuration(SLIDE_SWIPE_DURATION_MS)
+                            .setInterpolator(new DecelerateInterpolator()).start();
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float deltaY = event.getRawY() - mStartY;
+                    if (deltaY > 0){
+                        if(Math.abs(deltaY) > mSlop){
+                            mCard.setTranslationY(deltaY);
+                        }
+                    }
+                    break;
+                }
+                return true;
+            }
+        });
+
+        // slide in
+        mCard.setTranslationY(mCardHeight);
+        mCard.animate().translationY(0.0f).setDuration(SLIDE_IN_DURATION_MS)
             .setInterpolator(new DecelerateInterpolator()).start();
 
-        // Lookup contact info
-        startContactInfoSearch(identification);
+        // Lookup contact info - delay until the slide-in animation is done
+        // else if can be disturbed and we want it to look smooth
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                startContactInfoSearch(identification);
+            }
+        }, SLIDE_IN_DURATION_MS);
     }
 
     @Override
     public void onBackPressed() {
-        final Call call = CallList.getInstance().getIncomingCall();
-        if (call != null) {
-            InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, true);
-        }
-        super.onBackPressed();
+        // send to background
+        InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, true);
+        // slide out
+        hideCard();
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                InCallCardActivity.super.onBackPressed();
+            }
+        }, SLIDE_OUT_DURATION_MS);
+    }
+
+    private void hideCard() {
+        mCard.setTranslationY(0);
+        mCard.animate().translationY(mCardHeight).setDuration(SLIDE_OUT_DURATION_MS)
+                .setInterpolator(new AccelerateInterpolator()).start();
     }
 
     /**
@@ -108,11 +198,58 @@ public class InCallCardActivity extends Activity {
                 public void onImageLoadComplete(int callId, ContactCacheEntry entry) {
                     if (entry.photo != null) {
                         Drawable current = mContactImage.getDrawable();
-                        AnimationUtils.startCrossFade(mContactImage, current, entry.photo);
+                        int contactHeight = getResources().getDimensionPixelSize(R.dimen.incoming_call_card_contact_height);
+                        Drawable photo = resize(entry.photo, contactHeight);
+                        AnimationUtils.startCrossFade(mContactImage, current, photo);
                     }
                 }
             });
     }
 
+    @Override
+    public void onAnswer() {
+        InCallPresenter.getInstance().startIncomingCallUi(InCallPresenter.InCallState.INCALL, false);
+        CallCommandClient.getInstance().answerCall(mCall.getCallId());
+        finish();
+    }
+
+    @Override
+    public void onDecline() {
+        CallCommandClient.getInstance().rejectCall(mCall, false, null);
+        finishWithHide();
+    }
+
+    @Override
+    public void onText() {
+        // should not happen
+        finish();
+    }
+
+    private BitmapDrawable resize(Drawable image, int dpiSize) {
+        float density = getResources().getDisplayMetrics().density;
+        int size = Math.round(dpiSize * density);
+
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+
+        Bitmap bmResult = Bitmap.createBitmap(size, size,
+                Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(bmResult);
+
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, size,
+                size, true);
+        tempCanvas.drawBitmap(bitmapResized, 0, 0, null);
+        return new BitmapDrawable(getResources(), bmResult);
+    }
+
+    // finish with card hide animation
+    public void finishWithHide() {
+        // dismiss card
+        hideCard();
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                finish();
+            }
+        }, SLIDE_OUT_DURATION_MS);
+    }
 }
 
